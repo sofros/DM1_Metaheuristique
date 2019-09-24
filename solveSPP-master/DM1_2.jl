135509f13c0a6f3e32c0d871c1d30ce68cf45597
Emmanuel <erochet@outlook.fr>
2019-09-24 14:12:35 +0200

    Dm1_2 à test

--- solveSPP-master/DM1_2.jl
+++ solveSPP-master/DM1_2.jl
@@ -8,28 +8,39 @@
 #taking an array of lenght m
 #returning another array of length m
 
-<<<<<<< HEAD
-function kpexchange(args)
-    body
+
 =======
 function kpexchange(
     x::Array(Bool) # Notre solution
     k::Int  # le nombre d'objet à retirer du conteneur
     p::Int  # le nombre d'objet à rajouter dans le conteneur
-    m::Int  # taille de x
+    m::Int  # taille de x (inutile ici) TODO : virer ça
+    zbest::Int  # solution trouvée
+    z::Int #solution actuelle
+    couts::Array(Int) # tableau des coûts de x
+    xbest::Array(Bool) #meilleur solution trouvée
     )
 
-    i::Int
-    j::Int
-    ks::Array()
-    x_prime =copy(x)   # on copie notre solution
-    for n = 1:length(x_prime) - k
-        i = 0 # compteur k
-        pointeur = n # un pointeur tableau
-        for n = i:length(x)
-                if x_prime[n] == 1
+    if k > 0
+        for i in 1:length(x)
+            if x[i] == 1
+                x_prime = copy(x)
+                x_prime[i] = 0
+                return kp exchange(x_prime,k-1,p,m,zbest,z-gains[i],couts, gain, xbest)
+            end
+        elseif p > 0
+            for j in 1:length(x) && z += couts[j] < 1
+                if x[j] == 0  # TODO : mettre une condition pour vérifier que l'on sortpas de la région admissible
+                    x_prime = copy(x)
+                    x_prime[j] = 1
+                    z += couts[j]
+                    if z > zbest
+                        xbest = x_prime
+                        zbest = z
+                    end
+                    return kp exchange(x_prime,k,p-1,m,zbest,z+gains[i],couts, gain, xbest)
+                end
+            end
         end
     end
-
->>>>>>> master
 end
