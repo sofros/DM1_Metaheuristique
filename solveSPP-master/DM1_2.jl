ea712bd2bfac293c73188060fff51f9c59484a10
Emmanuel <erochet@outlook.fr>
2019-09-25 10:08:56 +0200

    optimisation de le fonction de kp_ex
    rajout d'un tableau pour passer la vérification en O(m)

--- solveSPP-master/DM1_2.jl
+++ solveSPP-master/DM1_2.jl
@@ -14,6 +14,7 @@ function calculz(x,costs,m)
     for i in 1:m
         z+= x[i]*costs[i]
     end
+
     return z
 end
 
@@ -21,65 +22,85 @@ function kpexchange!(
     x, # Notre solution
     k::Int,  # le nombre d'objet à retirer du conteneur
     p::Int, # le nombre d'objet à rajouter dans le conteneur
-    m::Int,  # taille de x
+    n::Int,  # taille de x
+    m::Int, # nombre de contraintes
     zbest::Int,  # solution trouvée
     z::Int, #solution actuelle
     couts, # tableau des coûts de x
-    xbest #meilleur solution trouvée
+    xbest, #meilleur solution trouvée
+    crts, # vecteur des contraintes (on suppose que pout tout i crts[i] =< 1)
+    matrix # la matrice des contraintes
     )
 println("===============kp exchange =====================")
-    sol = [xbest,zbest]
+    sol = (xbest,zbest)
+    println("x = ",x)
     if k > 0
         println("========= k =============")
-        for i in 1:m
+        for i in 1:n
             println("======================")
-            println("==> élément ",i," sur ",m)
+            println("==> élément ",i," sur ",n)
             println("======================")
             if x[i] == 1
                 println("======================")
-                println("==> x[i] = 1")
+                println("==> x[",i,"] = 1")
                 println("======================")
                 x_prime = copy(x)
                 println("==> copie effectuée")
                 x_prime[i] = 0
-                println("==> x[i] passé à 0")
+                println("==> x[",i,"] passé à 0")
+                for crt in 1:m
+                    if matrix[i,crt] == 1
+                        crts[crt] = 0
+                    end
+                end
                 println("======================")
                 println("Appel récursif k")
-                sol = kpexchange!(x_prime,k-1,p,m,zbest,z-couts[i],couts, xbest)
+                sol = kpexchange!(x_prime,k-1,p,n,m,zbest,z-couts[i],couts, xbest,crts)
             end
         end
     elseif p > 0
         println("========= p =============")
-        for j in m
+        for j in 1:n
             println("======================")
-            println("==> élément ",j," sur ",m)
+            println("==> élément ",j," sur ",n)
             println("======================")
             if x[j] == 0
                 println("======================")
-                println("==> x[j] = 0")
+                println("==> x[",j,"] = 0")
                 println("======================")
-                x_prime = copy(x)
-                println("==> copie effectuée")
-                x_prime[j] = 1
-                println("==> x[j] passé à 1")
-                println("======================")
-                println("Appel récursif")
-                z = calculz(x_prime,couts,m)
-                println("nouvelle solution",z)
-                if z != -1
-                    println("Z passe en meilleur solution")
-                    if z > zbest
+                selec = true # juste une initialisation
+                # ici on regarde si on peu rajouter notre variable
+                for l in 1:m
+                    selec = selec && ( crts[l] + matrix[j,l] >= 1)
+                end
+                println("crts =", crts, "" )
+                if selec
+                    x_prime = copy(x)
+                    println("==> copie effectuée")
+                    x_prime[j] = 1  # on rajoute la variable j à notre solution
+                    println("==> x[",j,"] passé à 1")
+                    println("======================")
+                    println("Appel récursif")
+                    z = calculz(x_prime,couts,m)
+                    println("nouvelle solution : z = ",z)
+                    if z > zbest #on regarde si on a amélioré notre solution
+                        println("Z passe en meilleur solution")
                         xbest = x_prime
                         zbest  = z
                     end
+                    for crt in 1:m # on met à jour nos contraintes
+                        if matrix[j,crt] == 1
+                            crts[crt] = 1
+                        end
+                    end
                 end
                 println("Appel récursif p")
-                sol = kpexchange!(x_prime,k,p-1,m,zbest,z,couts, xbest)
+                sol = kpexchange!(x_prime,k,p-1,n,m,zbest,z,couts, xbest,crts)
             end
         end
     else
+        sol = (xbest,zbest)
         println("solution : ", sol)
-
         return sol
     end
 end
