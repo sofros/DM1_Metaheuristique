18bd7e5e25258b930573277dfd54f6b5409f9db6
Nilson <n.toula@live.fr>
2019-11-13 16:47:00 +0100

    Merge remote-tracking branch 'Remote_DM1Meta/master'

---
+++ solveSPP-master/recuit2.jl
@@ -0,0 +1,36 @@
+
+t_deb = 1000000
+t_fin = 100
+alpha = 0.8
+palier = 50
+
+function acceptation(solution, delta, t)
+ return rand(Float64) <= Base.MathConstants.ℯ ^ (delta/t)
+end
+
+function SA(solution,n,m,couts,crts,matrix,mouvement)
+
+    x = Solution(solution,calculz(solution,couts,m))
+
+    bestsol = deepcopy(x)
+    t=t_deb
+
+    while t >= t_fin
+        for i in 1:palier
+            new_sol = deepcopy(x)
+            mouvement(new_sol.x,n,m,couts,crts,matrix)
+            #println("solitiion " ,i," : ",x.objectif)
+            if new_sol.objectif > bestsol.objectif
+                bestsol = new_sol
+                solution = new_sol
+            else
+                delta = new_sol.objectif - x.objectif
+                if acceptation(new_sol,delta,t)
+                    x = new_sol
+                end
+            end
+        end
+        t = t*alpha
+    end
+    return(bestsol,bestsol.objectif)
+end
